688f163b0b9e9908ac8e0c14e8ef318037588ca9
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-16 01:20:35 +0500

    Add condition to check if input field is empty

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -13,10 +13,14 @@ class InputTodo extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.addTodoProps(this.state.title);
-    this.setState({
-      title: ""
-    });
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title)
+      this.setState({
+        title: "",
+      });
+    } else {
+      alert("Please write item");
+    }
   };
 
   render() {
