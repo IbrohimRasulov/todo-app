231db1d5d7b859b585402f5935b38a9a209bfb1e
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-02-16 02:53:47 +0500

    Make tasks editable

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,31 +1,59 @@
+import React from 'react';
 import styles from './TodoItem.module.css';
+import { setState } from 'react';
 
-const TodoItem = (props) => {
-  const completedStyle = {
-    fontStyle: "italic",
-    color: "#595959",
-    opacity: 0.4,
-    textDecoration: "line-through",
+class TodoItem extends React.Component {
+  state = {
+    editing: false,
   }
 
-  const { completed, id, title } = props.todo
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    })
+  }
 
-  return (
-    <li className={styles.item}>
-      <input
-        type="checkbox"
-        className={styles.checkbox}
-        checked={completed}
-        onChange={() => props.handleChangeProps(id)}
-      />
-      <button onClick={() => props.deleteTodoProps(id)}>
-        Delete
-      </button>
-      <span style={completed ? completedStyle : null}>
-        {title}
-      </span>
-    </li>
-  );
+  render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
+
+    const { completed, id, title } = this.props.todo
+
+    let viewMode = {}
+    let editMode = {}
+
+
+
+    if (this.state.editing) {
+      viewMode.display = "none"
+    } else {
+      editMode.display = "none"
+    }
+
+    return (
+      <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={completed}
+            onChange={() => this.props.handleChangeProps(id)}
+          />
+          <button onClick={() => this.props.deleteTodoProps(id)}>
+            Delete
+          </button>
+          <span style={completed ? completedStyle : null}>
+            {title}
+          </span>
+        </div>
+        <input type="text" style={editMode} className={styles.textInput} />
+      </li>
+    );
+  }
 }
 
 export default TodoItem;
